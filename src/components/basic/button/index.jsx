709ca89e3442b17fd91d9e44e724914c5be99c29
Drakson Bezerra Santos <drakson_bezerra@virgo.atlantico.net.br>
2022-11-18 20:24:58 -0300

    feat: Nova tela de login

--- src/components/basic/button/index.jsx
+++ src/components/basic/button/index.jsx
@@ -1,16 +1,39 @@
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+
+// const ButtonMain = ({
+// 	title,
+// 	action
+// }) => {
+// 	return (
+// 		<Button
+// 			onClick={action}
+// 			variant="primary"
+// 		>
+//             {title}
+//         </Button>
+// 	)
+// }
+
+// export default ButtonMain
+
+import { MDBBtn } from 'mdb-react-ui-kit'
+import "./button.css"
 
 const ButtonMain = ({
 	title,
 	action
 }) => {
 	return (
-		<Button
+		<MDBBtn
+			id="MDBSteerBtn"
+			outline
+			className='mx-2 px-5'
+			color='white'
+			size='lg'
 			onClick={action}
-			variant="primary"
 		>
-            {title}
-        </Button>
+			{title}
+		</MDBBtn>
 	)
 }
 
