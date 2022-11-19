5d6072e25706ff1a4df71261e4212b825e76235a
Drakson Bezerra Santos <drakson_bezerra@virgo.atlantico.net.br>
2022-11-18 22:16:59 -0300

    feat: Adicionado troca de cor quando o filtro foi selecionado

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,12 +1,36 @@
 import "./filter.css"
+import nextId from "react-id-generator";
+import { useState } from "react";
 
-const FilterComponent = ({ icon, selected }) => {
+const filters_selected = [];
+
+function filterIsSelected(filter_id) {
+    return filters_selected.indexOf(filter_id) > -1
+}
+
+function select_filter(html_id) {
+    filters_selected.push(html_id)
+}
+
+function unselect_filter(html_id) {
+    filters_selected.splice(filters_selected.indexOf(html_id), 1);
+}
+
+const FilterComponent = ({ icon }) => {
+    const htmlId = nextId();
+    const [selected, setSelected] = useState(false)
+
+    const filterClicked = (html_id) => {
+        setSelected(!selected)
+    }
+
     return (
         <div
             id="Filter"
             className={
                 selected ? "filter-selected" : "filter-no-selected"
             }
+            onClick={() => filterClicked(htmlId)}
         >
             <img src={icon} alt="" />
         </div>
